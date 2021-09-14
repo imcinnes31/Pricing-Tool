4a8911cc82019351997d7ccf0b2a196802876610
Raymond Weng <xiang.weng3@gmail.com>
2021-09-13 22:35:18 -0400

    Update: Contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
-
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
 
 export default function Contact() {
-    return (
-        <div className='container'>
-            Contact us here
-        </div>
-    )
+  return (
+    <div className="container">
+        <h1 class="display-1 text-center">Contact Us</h1>
+      <Form>
+        <Form.Label>Name</Form.Label>
+        <Form.Group as={Row} className="mb-3" controlId="formFullName">
+          <Col>
+            <Form.Control type="firstname" placeholder="First name" required />
+          </Col>
+          <Col>
+            <Form.Control type="lastname" placeholder="Last name" required />
+          </Col>
+        </Form.Group>
+
+        <Form.Label>Email</Form.Label>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Control type="email" placeholder="Enter email" required />
+        </Form.Group>
+
+        <Form.Label>I Am A...</Form.Label>
+        <Form.Group className="mb-3" controlId="formIdentity">
+          <Form.Check
+            inline
+            required
+            type="radio"
+            label="Client"
+            name="formIdentityRadios"
+            id="formRadioClient"
+          />
+          <Form.Check
+            inline
+            required
+            type="radio"
+            label="Counsellor"
+            name="formIdentityRadios"
+            id="formRadioCounsellor"
+          />
+        </Form.Group>
+
+        <Form.Label>Message</Form.Label>
+        <Form.Group controlId="formMessage">
+          <Form.Control as="textarea" rows={3} required />
+        </Form.Group>
+
+        <Form.Label></Form.Label>{/* Hack: margin bottom for text-area */}
+        <Form.Group className="mb-3" controlId="formSubmit">
+          <button
+            type="submit"
+            className="btn primary-button w-100"
+            style={{ marginLeft: "0px" }}
+          >
+            Submit
+          </button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
