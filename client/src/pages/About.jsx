2a8aed16c38ed1737434c06c151a54371f402398
Raymond Weng <xiang.weng3@gmail.com>
2021-09-14 23:06:49 -0400

    Update: first half of About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { ABOUT_US_DATA } from "../constants/aboutUsData";
-
+import { ABOUT_US_DATA1, ABOUT_US_DATA2 } from "../constants/aboutUsData";
+import { Container, Row, Col } from "react-bootstrap";
 export default function About() {
   return (
-    <div className="container">
-      <div className="container  border-bottom">
-        <h1>About Us</h1>
-      </div>
+    <Container>
+      <Row>
+        <Col md={4}>
+          <h1 className="vertical-center">
+            <b>
+              Phare is a
+              <span style={{ color: "var(--secondary_1)" }}> community</span>, a
+              <span style={{ color: "var(--secondary_2)" }}> mission</span>, and
+              a<span style={{ color: "var(--secondary_3)" }}> revolution</span>.
+            </b>
+          </h1>
+        </Col>
 
-      <br />
+        <Col md={4}>
+          <h4 className="horizontal-center">{ABOUT_US_DATA1}</h4>
+        </Col>
+        <Col md={4}>
+          <h4 className="horizontal-center">{ABOUT_US_DATA2}</h4>
+        </Col>
+      </Row>
 
-      <div className="container">
-        <p>{ABOUT_US_DATA}</p>
-      </div>
-    </div>
+    </Container>
   );
 }
