c294a49d6126e2c4922a337df9136e29f2023fe6
Raymond Weng <xiang.weng3@gmail.com>
2021-10-13 15:48:08 -0400

    Implement UUID
    unique id for counselors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const server = express();
+const uuid = require("uuid");
 
 const CounselorModel = require("./database/models/counselor");
 // Checks the environment of the server to look for the config settings to check for the correct port. If it doesn't exist, use 5000 instead.
@@ -13,6 +14,7 @@ require('./routes/counselors.route.js')(server);
 
 server.post("/api/insertCounselor", async (req, res) =>{
   const counselor_data = req.body;
+  counselor_data.id = uuid.v4();
   try {
     await new CounselorModel(counselor_data).save();
   } catch (error) {
